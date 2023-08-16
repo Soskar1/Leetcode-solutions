7cc254332897ba13d2ca2dc8af230494ffef8437
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-08-16 13:37:17 +0300

    Find the Highest Altitude

---
+++ Arrays/Easy/Find the Highest Altitude.cs
@@ -0,0 +1,20 @@
+﻿//Problem: https://leetcode.com/problems/find-the-highest-altitude/
+
+public class Solution
+{
+    public int LargestAltitude(int[] gain)
+    {
+        int max = 0;
+        int currentAltitude = 0;
+
+        for (int i = 0; i < gain.Length; ++i)
+        {
+            currentAltitude += gain[i];
+
+            if (max < currentAltitude)
+                max = currentAltitude;
+        }
+
+        return max;
+    }
+}
