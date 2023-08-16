44a6f47ad9c2f217b13d224d2acaf6c4f7f9bdf4
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-08-16 15:13:27 +0300

    Longest Consecutive Sequence

---
+++ Arrays/Medium/Longest Consecutive Sequence.cs
@@ -0,0 +1,35 @@
+﻿//Problem: https://leetcode.com/problems/longest-consecutive-sequence/
+
+class Solution
+{
+    public int LongestConsecutive(int[] nums)
+    {
+        if (nums.Length <= 1)
+            return 0;
+
+        int maxCount = 0;
+        int currentCount = 1;
+
+        nums = nums.Distinct().OrderBy(x => x).ToArray();
+
+        for (int i = 0; i < nums.Length; ++i)
+        {
+            if (i + 1 == nums.Length)
+                break;
+
+            if (nums[i + 1] - nums[i] == 1)
+            {
+                ++currentCount;
+
+                if (maxCount < currentCount)
+                    maxCount = currentCount;
+            }
+            else
+            {
+                currentCount = 1;
+            }
+        }
+
+        return maxCount;
+    }
+}
