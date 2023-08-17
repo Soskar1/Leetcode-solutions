45b8fee9e938e0e7de14bfe9eccce0e2379ba143
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2023-08-17 11:06:20 +0300

    Stack | Easy | Valid Parentheses

---
+++ Stack/Easy/Valid Parentheses.cs
@@ -0,0 +1,39 @@
+﻿//Problem: https://leetcode.com/problems/valid-parentheses/
+
+public class Solution
+{
+    Dictionary<char, char> validParentheses = new Dictionary<char, char>
+    {
+        { ')', '(' },
+        { '}', '{' },
+        { ']', '[' },
+    };
+
+    public bool IsValid(string s)
+    {
+        Stack<char> openParentheses = new Stack<char>();
+
+        foreach (char c in s)
+        {
+            if (validParentheses.ContainsKey(c))
+            {
+                if (openParentheses.Count == 0)
+                    return false;
+
+                char bracket = openParentheses.Pop();
+
+                if (bracket != validParentheses[c])
+                    return false;
+            }
+            else
+            {
+                openParentheses.Push(c);
+            }
+        }
+
+        if (openParentheses.Count > 0)
+            return false;
+
+        return true;
+    }
+}
