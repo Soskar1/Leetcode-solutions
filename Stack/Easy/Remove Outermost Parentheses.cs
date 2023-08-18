93291ce74023a23e1edd77bd625116d35e71f613
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2023-08-18 10:31:19 +0300

    Stack | Easy | Remove Outermost Parentheses

---
+++ Stack/Easy/Remove Outermost Parentheses.cs
@@ -0,0 +1,38 @@
+﻿//Problem: https://leetcode.com/problems/remove-outermost-parentheses/
+
+using System.Text;
+
+public class Solution
+{
+    public string RemoveOuterParentheses(string s)
+    {
+        StringBuilder sb = new StringBuilder();
+        int openBracketsCount = 0;
+        int startIndex = 0;
+
+        for (int i = 0; i < s.Length; ++i) 
+        {
+            sb.Append(s[i]);
+
+            if (s[i] == '(')
+            {
+                ++openBracketsCount;
+
+                if (openBracketsCount == 1)
+                    startIndex = sb.Length - 1;
+            }
+            else
+            {
+                --openBracketsCount;
+
+                if (openBracketsCount == 0)
+                {
+                    sb.Remove(sb.Length - 1, 1);
+                    sb.Remove(startIndex, 1);
+                }
+            }
+        }
+
+        return sb.ToString();
+    }
+}
