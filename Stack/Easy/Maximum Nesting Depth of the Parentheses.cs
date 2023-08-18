e6374afab0ee8c2215c47c7a0b4d4435088d465e
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2023-08-18 10:13:30 +0300

    Stack | Easy | Maximum Nesting Depth of the Parentheses

---
+++ Stack/Easy/Maximum Nesting Depth of the Parentheses.cs
@@ -0,0 +1,33 @@
+﻿//Problem: https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
+
+public class Solution
+{
+    public int MaxDepth(string s)
+    {
+        Stack<char> openBrackets = new Stack<char>();
+        int maxDepth = 0;
+
+        foreach (char c in s)
+        {
+            if (c == '(')
+            {
+                openBrackets.Push(c);
+
+                if (maxDepth < openBrackets.Count)
+                    maxDepth = openBrackets.Count;
+            }
+            else if (c == ')')
+            {
+                if (openBrackets.Count == 0)
+                    continue;
+
+                openBrackets.Pop();
+            }
+        }
+
+        if (openBrackets.Count > 0)
+            return 0;
+
+        return maxDepth;
+    }
+}
