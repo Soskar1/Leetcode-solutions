ed2504365419d9353d8eecf681150b8e5872bee5
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2023-08-20 10:51:21 +0300

    Stack | Medium | Daily Temperatures | C#

---
+++ Stack/Medium/Daily Temperatures.cs
@@ -0,0 +1,29 @@
+﻿//Problem: https://leetcode.com/problems/daily-temperatures/
+
+public class Solution
+{
+    public int[] DailyTemperatures(int[] temperatures)
+    {
+        Stack<int> monoStack = new Stack<int>();
+        int[] daysToWait = new int[temperatures.Length];
+
+        for (int i = 0; i < temperatures.Length; ++i)
+        {
+            int currentTemperature = temperatures[i];
+            int dayDiff = 1;
+
+            while (monoStack.Count > 0 && currentTemperature > monoStack.Peek())
+            {
+                while (daysToWait[i - dayDiff] != 0)
+                    ++dayDiff;
+
+                daysToWait[i - dayDiff] = dayDiff;
+                monoStack.Pop();
+            }
+
+            monoStack.Push(currentTemperature);
+        }
+
+        return daysToWait;
+    }
+}
