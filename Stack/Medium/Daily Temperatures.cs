aed88d680402bab2c1ec1b6516b8a52f04e741c2
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2023-08-20 11:11:10 +0300

    Daily Temperatures better solution

--- Stack/Medium/Daily Temperatures.cs
+++ Stack/Medium/Daily Temperatures.cs
@@ -27,3 +27,28 @@ public class Solution
         return daysToWait;
     }
 }
+
+//Better solution
+public class Solution2
+{
+    public int[] DailyTemperatures(int[] temperatures)
+    {
+        Stack<int> monoStack = new Stack<int>();
+        int[] output = new int[temperatures.Length];
+
+        for (int i = 0; i < temperatures.Length; ++i)
+        {
+            int currentTemperature = temperatures[i];
+
+            while (monoStack.Count > 0 && currentTemperature > temperatures[monoStack.Peek()])
+            {
+                int dayIndex = monoStack.Pop();
+                output[dayIndex] = i - dayIndex;
+            }
+
+            monoStack.Push(i);
+        }
+
+        return output;
+    }
+}
