9496609284c3f12e4cfc18db87488dd37dad625d
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-08-21 14:05:06 +0300

    Stack | Easy | Final Prices With a Special Discount in a Shop | C#

---
+++ Stack/Easy/Final Prices With a Special Discount in a Shop.cs
@@ -0,0 +1,19 @@
+﻿//Problem: https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
+
+public class Solution
+{
+    public int[] FinalPrices(int[] prices)
+    {
+        Stack<int> indices = new Stack<int>();
+
+        for (int i = 0; i < prices.Length; ++i)
+        {
+            while (indices.Count > 0 && prices[indices.Peek()] - prices[i] >= 0)
+                prices[indices.Pop()] -= prices[i];
+
+            indices.Push(i);
+        }
+
+        return prices;
+    }
+}
