00de4565acb3356f40dd281e1ee71e0588251e6d
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2023-08-22 11:49:45 +0300

    Stack | Medium | Car Fleet | C#

---
+++ Stack/Medium/Car Fleet.cs
@@ -0,0 +1,23 @@
+﻿//Problem: https://leetcode.com/problems/car-fleet/
+//The solution is borrowed from: https://leetcode.com/problems/car-fleet/solutions/3615219/monotonic-stack/
+
+public class Solution
+{
+    public int CarFleet(int target, int[] position, int[] speed)
+    {
+        Stack<double> stack = new Stack<double>();
+        Array.Sort(position, speed);
+
+        for (int i = 0; i < position.Length; ++i)
+        {
+            double time = (double)(target - position[i]) / speed[i];
+
+            while (stack.Any() && time >= stack.Peek())
+                stack.Pop();
+
+            stack.Push(time);
+        }
+
+        return stack.Count;
+    }
+}
