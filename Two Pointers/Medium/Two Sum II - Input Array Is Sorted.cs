a130b6a76071f52152120e03024517709a78451e
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2023-08-24 12:00:06 +0300

    Two Pointers | Medium | Two Sum II | C#

---
+++ Two Pointers/Medium/Two Sum II - Input Array Is Sorted.cs
@@ -0,0 +1,27 @@
+﻿//Problem: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        int i = 0;
+        int j = numbers.Length - 1;
+        int sum = 0;
+
+        while (i < j)
+        {
+            sum = numbers[i] + numbers[j];
+
+            if (sum == target)
+                break;
+
+            if (sum < target)
+                ++i;
+
+            if (sum > target)
+                --j;
+        }
+
+        return new int[] { i + 1, j + 1 };
+    }
+}
