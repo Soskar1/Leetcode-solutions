ba1ec7a0add8df608033b595a76339ca9ead22f7
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2023-08-24 11:52:02 +0300

    Two Pointers | Easy | Valid Palindrome | C#

---
+++ Two Pointers/Easy/Valid Palindrome.cs
@@ -0,0 +1,37 @@
+﻿//Problem: https://leetcode.com/problems/valid-palindrome/
+
+public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        int i = 0;
+        int j = s.Length - 1;
+
+        while (i < j)
+        {
+            while (!Char.IsLetterOrDigit(s[i]))
+            {
+                ++i;
+
+                if (i > j)
+                    break;
+            }
+
+            while (!Char.IsLetterOrDigit(s[j]))
+            {
+                --j;
+
+                if (i > j)
+                    break;
+            }
+
+            if (i < j && Char.ToLower(s[i]) != Char.ToLower(s[j]))
+                return false;
+
+            ++i;
+            --j;
+        }
+
+        return true;
+    }
+}
