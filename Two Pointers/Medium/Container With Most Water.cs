676714a5e2c55efc0910fbbb25ec984914d2e9c6
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-08-25 14:19:04 +0300

    Two Pointers | Medium | Container With Most Water | C#

---
+++ Two Pointers/Medium/Container With Most Water.cs
@@ -0,0 +1,27 @@
+﻿//Problem: https://leetcode.com/problems/container-with-most-water/
+
+public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.Length - 1;
+
+        while (left < right)
+        {
+            if (height[left] <= height[right])
+            {
+                maxArea = Math.Max(maxArea, (right - left) * height[left]);
+                ++left;
+            }
+            else
+            {
+                maxArea = Math.Max(maxArea, (right - left) * height[right]);
+                --right;
+            }
+        }
+
+        return maxArea;
+    }
+}
