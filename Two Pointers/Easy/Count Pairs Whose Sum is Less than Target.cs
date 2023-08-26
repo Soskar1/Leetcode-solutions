15881e0f9ca15349e35c96948f5c6906c3a4457f
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2023-08-26 10:03:29 +0300

    Two Pointers | Easy | Count Pairs Whose Sum is Less tham Target | C#

---
+++ Two Pointers/Easy/Count Pairs Whose Sum is Less than Target.cs
@@ -0,0 +1,25 @@
+﻿//Problem: https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/
+
+public class Solution
+{
+    public int CountPairs(IList<int> nums, int target)
+    {
+        int output = 0;
+        int j;
+
+        for (int i = 0; i < nums.Count - 1; ++i)
+        {
+            j = nums.Count - 1;
+
+            while (i < j)
+            {
+                if (nums[i] + nums[j] < target)
+                    ++output;
+
+                --j;
+            }
+        }
+
+        return output;
+    }
+}
