9a6eff95f4338b0a56e84bc798cd92f1b8d03453
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2023-08-26 10:41:44 +0300

    Two Pointers | Easy | Reverse Words in a String III | C#

---
+++ Two Pointers/Easy/Reverse Words in a String III.cs
@@ -0,0 +1,24 @@
+﻿//Problem: https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+using System.Text;
+
+public class Solution
+{
+    public string ReverseWords(string s)
+    {
+        StringBuilder sb = new StringBuilder();
+        string[] words = s.Split(' ');
+
+        foreach (string word in words)
+        {
+            char[] tmp = word.ToCharArray();
+            Array.Reverse(tmp);
+            sb.Append(new string(tmp));
+            sb.Append(' ');
+        }
+
+        sb.Remove(sb.Length - 1, 1);
+
+        return sb.ToString();
+    }
+}
