0c587acde5b2ba7d64870a74d267cf0ffa647c31
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-08-28 12:25:28 +0300

    Sliding Window | Easy | Minimum Recolors to Get K Consecutive Black Blocks | C#

---
+++ Sliding Window/Easy/Minimum Recolors to Get K Consecutive Black Blocks.cs
@@ -0,0 +1,37 @@
+﻿//Problem: https://leetcode.com/problems/minimum-recolors-to-get-k-consecutive-black-blocks/
+
+public class Solution
+{
+    private const char _WHITE_ = 'W';
+
+    public int MinimumRecolors(string blocks, int k)
+    {
+        int current = 0;
+        int min;
+
+        for (int i = 0; i < k; ++i)
+            if (blocks[i] == _WHITE_)
+                ++current;
+
+        min = current;
+
+        if (min == 0)
+            return min;
+
+        for (int i = 0; i < blocks.Length - k; ++i)
+        {
+            if (blocks[i] == _WHITE_)
+                --current;
+
+            if (blocks[i + k] == _WHITE_)
+                ++current;
+
+            min = Math.Min(min, current);
+
+            if (min == 0)
+                break;
+        }
+
+        return min;
+    }
+}
