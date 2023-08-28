9888cc5c2392188795e8116fc2278f65146a2260
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-08-28 13:20:22 +0300

    Sliding Window | Easy | Contains Duplicate II | C#

---
+++ Sliding Window/Easy/Contains Duplicate II.cs
@@ -0,0 +1,39 @@
+﻿//Problem: https://leetcode.com/problems/contains-duplicate-ii/
+
+public class Solution
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        if (k == 0)
+            return false;
+
+        Dictionary<int, int> amountOfNumbers = new Dictionary<int, int>();
+
+        for (int i = 0; i <= k && i < nums.Length; ++i)
+        {
+            if (!amountOfNumbers.ContainsKey(nums[i]))
+                amountOfNumbers.Add(nums[i], 1);
+            else if (amountOfNumbers.ContainsKey(nums[i]))
+                return true;
+        }
+
+        for (int i = 0; i < nums.Length - k - 1; ++i)
+        {
+            --amountOfNumbers[nums[i]];
+
+            int rightIndex = i + k + 1;
+            if (amountOfNumbers.ContainsKey(nums[rightIndex]))
+            {
+                ++amountOfNumbers[nums[rightIndex]];
+                if (amountOfNumbers[nums[rightIndex]] > 1)
+                    return true;
+            }
+            else
+            {
+                amountOfNumbers.Add(nums[rightIndex], 1);
+            }
+        }
+
+        return false;
+    }
+}
