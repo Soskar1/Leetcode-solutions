4783a6e22a3f3e89d65a81b7bdac73d42bdf8bea
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-08-28 12:37:40 +0300

    Sliding Window

---
+++ Sliding Window/Easy/Minimum Difference Between Highest and Lowest of K Scores.cs
@@ -0,0 +1,23 @@
+﻿//Problem: https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/
+
+public class Solution
+{
+    public int MinimumDifference(int[] nums, int k)
+    {
+        int minDiff;
+        int currentDiff;
+
+        Array.Sort(nums);
+
+        currentDiff = nums[k - 1] - nums[0];
+        minDiff = currentDiff;
+
+        for (int i = 0; i < nums.Length - k; ++i)
+        {
+            currentDiff = nums[i + k] - nums[i + 1];
+            minDiff = Math.Min(minDiff, currentDiff);
+        }
+
+        return minDiff;
+    }
+}
