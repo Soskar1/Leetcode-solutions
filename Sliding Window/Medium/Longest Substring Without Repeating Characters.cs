c89edd067799c73c0f9d5156d42550925dd1ef9b
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2023-08-29 11:53:56 +0300

    Sliding Window | Medium | Longest Substring Without Repeating Characters | C#

---
+++ Sliding Window/Medium/Longest Substring Without Repeating Characters.cs
@@ -0,0 +1,32 @@
+﻿//Problem: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        int start = 0;
+        int maxLength = 0;
+        Dictionary<char, int> charactersInWindow = new Dictionary<char, int>();
+
+        for (int i = 0; i < s.Length; ++i)
+        {
+            if (!charactersInWindow.ContainsKey(s[i]))
+            {
+                charactersInWindow.Add(s[i], i);
+            }
+            else
+            {
+                maxLength = Math.Max(maxLength, i - start);
+
+                if (charactersInWindow[s[i]] >= start)
+                    start = charactersInWindow[s[i]] + 1;
+
+                charactersInWindow[s[i]] = i;
+            }
+        }
+
+        maxLength = Math.Max(maxLength, s.Length - start);
+
+        return maxLength;
+    }
+}
