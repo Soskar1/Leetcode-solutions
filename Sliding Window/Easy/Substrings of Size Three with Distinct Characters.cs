f5e4c1c2791ad1f81fb8d2c5d71a76a7c2a53c9f
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2023-09-01 10:02:42 +0300

    Sliding Window | Easy | Substrings of Size Three with Distinct Characters | C#

---
+++ Sliding Window/Easy/Substrings of Size Three with Distinct Characters.cs
@@ -0,0 +1,66 @@
+﻿//Problem: https://leetcode.com/problems/substrings-of-size-three-with-distinct-characters/
+
+public class Solution
+{
+    private const int WINDOW_SIZE = 3;
+
+    public int CountGoodSubstrings(string s)
+    {
+        if (s.Length < WINDOW_SIZE)
+            return 0;
+
+        Dictionary<char, int> freqMap = new Dictionary<char, int>();
+        int substringCount = 0;
+        int validCharacterCount = 0;
+
+        for (int i = 0; i < WINDOW_SIZE; i++)
+        {
+            if (!freqMap.ContainsKey(s[i]))
+            {
+                ++validCharacterCount;
+                freqMap.Add(s[i], 1);
+            }
+            else
+            {
+                ++freqMap[s[i]];
+
+                if (freqMap[s[i]] > 1)
+                    --validCharacterCount;
+            }
+        }
+
+        if (validCharacterCount == WINDOW_SIZE)
+            ++substringCount;
+
+        for (int i = 0; i + WINDOW_SIZE < s.Length; ++i)
+        {
+            --freqMap[s[i]];
+
+            if (freqMap[s[i]] >= 1)
+                ++validCharacterCount;
+            else
+                --validCharacterCount;
+
+            int rightIndex = i + WINDOW_SIZE;
+            if (!freqMap.ContainsKey(s[rightIndex]))
+            {
+                ++validCharacterCount;
+                freqMap.Add(s[rightIndex], 1);
+            }
+            else
+            {
+                ++freqMap[s[rightIndex]];
+
+                if (freqMap[s[rightIndex]] > 1)
+                    --validCharacterCount;
+                else
+                    ++validCharacterCount;
+            }
+
+            if (validCharacterCount == WINDOW_SIZE)
+                ++substringCount;
+        }
+
+        return substringCount;
+    }
+}
