23dca75b008d2039dba314648c9b11487357ad2f
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-09-02 12:13:40 +0300

    Sliding Window | Easy | Find the K-Beauty of a Number | C#

---
+++ Sliding Window/Easy/Find the K-Beauty of a Number.cs
@@ -0,0 +1,24 @@
+﻿//Problem: https://leetcode.com/problems/find-the-k-beauty-of-a-number/
+
+public class Solution
+{
+    public int DivisorSubstrings(int num, int k)
+    {
+        string strNum = num.ToString();
+        int output = 0;
+
+        for (int i = 0; i + k <= strNum.Length; ++i)
+        {
+            string substring = strNum.Substring(i, k);
+            int tmp = Int32.Parse(substring);
+
+            if (tmp == 0)
+                continue;
+
+            if (num % tmp == 0)
+                ++output;
+        }
+
+        return output;
+    }
+}
