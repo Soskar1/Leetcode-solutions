338bb439beb85a1e6fb8208dee3e0d4da0be4c67
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-09-03 13:04:29 +0300

    Linked List | Easy | Reverse Linked List | C#

---
+++ Linked List/Easy/Reverse Linked List.cs
@@ -0,0 +1,34 @@
+﻿//Problem: https://leetcode.com/problems/reverse-linked-list/
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;     
+    }
+}
+
+public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        ListNode previous = null;
+        List<ListNode> list = new List<ListNode>();
+
+        while (head != null)
+        {
+            list.Add(head);
+            previous = head;
+            head = head.next;
+            previous.next = null;
+        }
+
+        for (int i = list.Count - 1; i > 0; --i)
+            list[i].next = list[i - 1]; 
+
+        return previous;
+    }
+}
